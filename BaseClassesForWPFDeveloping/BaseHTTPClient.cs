b50c9d03de740b301807f467e37ecadfea257c53
Ihor Reva <ihor.reva@precast.pl>
2022-09-07 08:10:55 +0200

    Add project files.

---
+++ BaseClassesForWPFDeveloping/BaseHTTPClient.cs
@@ -0,0 +1,44 @@
+﻿namespace BaseClassesForWPFDeveloping
+{
+    using Newtonsoft.Json;
+    using System.Net.Http.Headers;
+    using System.Text;
+
+    public abstract class ClientBase
+    {
+        private readonly string webAppAdress;
+        private readonly string scheme;
+        private readonly string token;
+        protected StringBuilder actionAddressBuilder;
+
+        public ClientBase(string WebAppAdress, string Scheme, string Token)
+        {
+            webAppAdress = WebAppAdress;
+            scheme = Scheme;
+            token = Token;
+            actionAddressBuilder = new StringBuilder(webAppAdress);
+        }
+
+        protected HttpResponseMessage GetResponseFromHttp(HttpMethod method, object body)
+        {
+            HttpResponseMessage message;
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(scheme, token);
+                var request = new HttpRequestMessage()
+                {
+                    Method = method,
+                    RequestUri = new Uri(actionAddressBuilder.ToString())
+                };
+                actionAddressBuilder.Clear().Append(webAppAdress);
+                if (body != null)
+                {
+                    string data = JsonConvert.SerializeObject(body);
+                    request.Content = new StringContent(data, Encoding.UTF8, "application/json");
+                }
+                message = client.SendAsync(request).Result;
+            }
+            return message;
+        }
+    }
+}
---
+++ BaseClassesForWPFDeveloping/BaseViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BaseClassesForWPFDeveloping
+{
+    using System.ComponentModel;
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
